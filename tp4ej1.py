8a8d860f6f38582646a3b02770d04daf11bbf88e
Eimi Saiz <83459802+EimiSaiz@users.noreply.github.com>
2021-07-01 13:48:03 -0300

    Update tp4ej1.py

--- tp4ej1.py
+++ tp4ej1.py
@@ -4,18 +4,47 @@
 # UNRN Andina - Introducción a Ingeniería en Computación
 ########################################################
 
-def ingreso_entero(mensaje):
-    ingreso = input(mensaje + "Ingrese un número.\n")
-    try:
-        entero = int(ingreso)
-    except ValueError as err:
-        raise IngresoIncorrecto("No es un número.") from err
-    return entero
+def ingreso_entero_reintento():
+    intentos = 0
+    while intentos < 5:
+        valor = input("Ingrese un número entero.\n")
+        try:
+            valor = int(valor)
+            return valor
+        except ValueError as err:
+            intentos = intentos + 1
+            raise IngresoIncorrecto("Lo siento, no entendí.\n") from err
+    if intentos == 5:
+        print("Se superó la cantidad máxima de intentos.\n")
+    else:
+        pass
 
+def ingreso_entero_restringido(numero, valorA, valorB):
+    numero_mayor = 0
+    numero_menor = 0
+    if valorA < valorB:
+        numero_mayor = valorB
+        numero_menor = valorA
+    else:
+        numero_mayor = valorA
+        numero_menor = valorB
+    if numero >= numero_menor and numero <= numero_mayor:
+        return True
+    else:
+        return False
 
 def prueba():
-    ingreso_entero("Ingresar un numero entero.\n")
-    pass
-
+    valorA = int(input("Ingrese un número.\n"))
+    valorB = int(input("Ingrese otro número.\n"))
+    numero = int(input("Ingrese un número entre los números anteriormente ingresados.\n"))
+    resultado_restrigido = ingreso_entero_restringido(numero, valorA, valorB)
+    resultado_reintento = ingreso_entero_reintento()
+    
+    ingreso_entero_restringido(numero, valorA, valorB)
+    ingreso_entero_reintento()
+    
+    print(resultado_restrigido)
+    print(resultado_reintento)
+    
 if __name__ == "__main__":
     prueba()
